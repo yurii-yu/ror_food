96f8032da69d005b71ffbbc345ff42fbc7077903
yurii-yu <yusheng.email@gmail.com>
2014-07-08 20:23:40 +0800

    rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Food::Application.config.secret_key_base = '54685e24db57ac961e16829b3a504cd7b8da89750285e9f207786aeac997598a50ad9e993a38e63c5594268f1d69553bcc3e88065c9a1c0072b868787071536e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Food::Application.config.session_store :cookie_store, key: '_food_session'
